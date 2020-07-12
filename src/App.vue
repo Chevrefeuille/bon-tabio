9c07b5d7c7a7c3d5ea4281cb8a25f8da8ae78583
seriseriseri <serina8910@gmail.com>
2020-07-12 20:06:24 +0900

    delete vue logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Index msg="Welcome to Your Vue.js App"/>
     <Index />
   </div>
 </template>
@@ -24,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
