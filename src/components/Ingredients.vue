ea9e3b9bcb40551eeef48c9893e2d0dd4979bfa4
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2020-09-20 16:57:12 +0200

    add variable amount for the recipe

---
+++ src/components/Ingredients.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ingredients">
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+      <li
+        v-for="(ingredient, i) in $page.recipe.ingredients"
+        :key="i"
+      >{{ ingredient.name }} : {{ ingredient.quantity }} {{ ingredient.unit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ingredients",
+  data() {
+    return {
+      quantity: 1,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/templates/Recipe.vue
+++ src/templates/Recipe.vue
@@ -2,16 +2,18 @@
   <Layout>
     <g-link to="/recipes">Go back to the list of recipes</g-link>
     <h2>{{ $page.recipe.name }}</h2>
-    <div class="ingredients">
+    For {{ amount }} {{$page.recipe.amount.unit }}
+    <a v-on:click="amount += 1">+</a>/<a v-on:click="amount = Math.max(1, amount - 1)">-</a>
+    <div class="box">
       <h3>Ingredients</h3>
       <ul class="ingredients">
         <li
           v-for="(ingredient, i) in $page.recipe.ingredients"
           :key="i"
-        >{{ ingredient.name }} : {{ ingredient.quantity }}</li>
+        >{{ ingredient.name }} : {{ scaleQuantity(ingredient.quantity) }} {{ ingredient.unit }}</li>
       </ul>
     </div>
-    <div class="steps">
+    <div class="box">
       <h3>Steps</h3>
       <ul class="steps">
         <li v-for="(step, i) in $page.recipe.steps" :key="i">{{ step.action }}</li>
@@ -20,14 +22,38 @@
   </Layout>
 </template>
 
+<script>
+export default {
+  name: "Recipe",
+  data() {
+    return {
+      amount: 0
+    };
+  },
+  mounted () {
+    this.amount = this.$page.recipe.amount.quantity;
+  },
+  methods: {
+    scaleQuantity(quantity) {
+      return this.amount * (quantity / this.$page.recipe.amount.quantity);
+    }
+  },
+};
+</script>
+
 <page-query>
 query ($id: ID!) {
   recipe(id: $id) {
     name
     image
+    amount {
+      quantity
+      unit
+    }
     ingredients {
       name
       quantity
+      unit
     }
     steps {
       name
