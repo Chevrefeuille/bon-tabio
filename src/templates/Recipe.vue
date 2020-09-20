b832450e85a1b1292f5becef9e3b346ab4efacf1
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2020-09-20 17:06:36 +0200

    Change layout of recipe

--- src/templates/Recipe.vue
+++ src/templates/Recipe.vue
@@ -3,27 +3,34 @@
     <g-link to="/recipes">Go back to the list of recipes</g-link>
     <h2>{{ $page.recipe.name }}</h2>
     For {{ amount }} {{$page.recipe.amount.unit }}
-    <a v-on:click="amount += 1">+</a>/<a v-on:click="amount = Math.max(1, amount - 1)">-</a>
-    <div class="box">
-      <h3>Ingredients</h3>
-      <ul class="ingredients">
-        <li
-          v-for="(ingredient, i) in $page.recipe.ingredients"
-          :key="i"
-        >{{ ingredient.name }} : {{ scaleQuantity(ingredient.quantity) }} {{ ingredient.unit }}</li>
-      </ul>
-      <ul class="ingredients">
-        <li
-          v-for="(ingredient, i) in $page.recipe.otherIngredients"
-          :key="i"
-        >{{ ingredient.name }}</li>
-      </ul>
-    </div>
-    <div class="box">
-      <h3>Steps</h3>
-      <ul class="steps">
-        <li v-for="(step, i) in $page.recipe.steps" :key="i">{{ step.action }}</li>
-      </ul>
+    <a v-on:click="amount += 1">+</a>/
+    <a v-on:click="amount = Math.max(1, amount - 1)">-</a>
+    <div class="columns">
+      <div class="column is-one-third">
+        <div class="box">
+          <h3>Ingredients</h3>
+          <ul class="ingredients">
+            <li
+              v-for="(ingredient, i) in $page.recipe.ingredients"
+              :key="i"
+            >{{ ingredient.name }} : {{ scaleQuantity(ingredient.quantity) }} {{ ingredient.unit }}</li>
+          </ul>
+          <ul class="ingredients">
+            <li
+              v-for="(ingredient, i) in $page.recipe.otherIngredients"
+              :key="i"
+            >{{ ingredient.name }}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          <h3>Steps</h3>
+          <ul class="steps">
+            <li v-for="(step, i) in $page.recipe.steps" :key="i">{{ step.action }}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
@@ -33,16 +40,16 @@ export default {
   name: "Recipe",
   data() {
     return {
-      amount: 0
+      amount: 0,
     };
   },
-  mounted () {
+  mounted() {
     this.amount = this.$page.recipe.amount.quantity;
   },
   methods: {
     scaleQuantity(quantity) {
       return this.amount * (quantity / this.$page.recipe.amount.quantity);
-    }
+    },
   },
 };
 </script>
