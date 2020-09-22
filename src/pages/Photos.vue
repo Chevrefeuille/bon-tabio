b9c28dbe20d55746abff18e58afde6530f09055d
serina <serina8910@gmail.com>
2020-09-22 17:42:45 +0900

    ...

--- src/pages/Photos.vue
+++ src/pages/Photos.vue
@@ -1,5 +1,28 @@
 <template>
   <Layout>
-    <h1>This is our GREAT photo page</h1>
+    <section class="photos">
+      <div>
+        <h1>This is our GREAT photo page</h1>
+      </div>
+      <div class="container images">
+        <figure class="is-clickable">
+          <img src="../assets/shiba.jpg" />
+        </figure>
+      </div>
+    </section>
   </Layout>
 </template>
+
+<style>
+.photos {
+  background-image: url("../assets/image_4.jpg");
+  background-size: cover;
+}
+.images {
+  display: flex;
+  justify-content: center;
+}
+.is-clickable {
+  cursor: pointer;
+}
+</style>
