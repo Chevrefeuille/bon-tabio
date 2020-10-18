b215c25817475537d61b8d78867788b23803186a
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2020-10-18 22:37:18 +0200

    Edit recipe style

--- src/templates/Recipe.vue
+++ src/templates/Recipe.vue
@@ -1,65 +1,53 @@
 <template>
   <Layout>
     <g-link to="/recipes">Go back to the list of recipes</g-link>
-    <h1 class="title has-text-weight-bold">{{ $page.recipe.name }}</h1>
-    <div class="columns recipe-content">
-      <div class="column is-4">
-        <div class="icon-and-text">
-          For {{ amount }} {{ $page.recipe.amount.unit }}
+    <div class="recipe-content">
+      <p class="title is-3 recipe-title">{{ $page.recipe.name }}</p>
+      <div class="columns ingredients-and-image">
+        <div class="column is-6">
           <div class="icon-and-text">
+            For {{ amount }} {{ $page.recipe.amount.unit }}
             <a v-on:click="amount = Math.max(1, amount - 1)">
-              <b-icon icon="minus-circle" size="is-medium"> </b-icon>
+              <b-icon icon="minus-circle" size="is-small"> </b-icon>
             </a>
             <a v-on:click="amount += 1">
-              <b-icon icon="plus-circle" size="is-medium"> </b-icon>
+              <b-icon icon="plus-circle" size="is-small"> </b-icon>
             </a>
           </div>
-        </div>
-        <!-- <div class="box" style="background-color:#ffc0cb"> -->
-        <div class="icon-and-text">
-          <b-icon icon="square" size="is-small"> </b-icon>
-          <h3>Ingredients</h3>
-        </div>
-        <div class="content">
-          <div class="ingredients">
-            <!-- <ul class="ingredients"> -->
-            <li v-for="(ingredient, i) in $page.recipe.ingredients" :key="i">
-              {{ ingredient.name }} : {{ scaleQuantity(ingredient.quantity) }}
-              {{ ingredient.unit }}
-            </li>
-            <!-- </ul> -->
-            <!-- <ul class="ingredients"> -->
-            <li
-              v-for="(ingredient, i) in $page.recipe.otherIngredients"
-              :key="i"
-            >
-              {{ ingredient.name }}
-            </li>
-            <!-- </ul> -->
+          <p class="title is-4 ingredients-title">Ingredients</p>
+          <div class="content">
+            <ul class="ingredients">
+              <li v-for="(ingredient, i) in $page.recipe.ingredients" :key="i">
+                {{ ingredient.name }} : {{ scaleQuantity(ingredient.quantity) }}
+                {{ ingredient.unit }}
+              </li>
+            </ul>
+            <ul class="ingredients-no-amount">
+              <li
+                v-for="(ingredient, i) in $page.recipe.otherIngredients"
+                :key="i"
+              >
+                {{ ingredient.name }}
+              </li>
+            </ul>
           </div>
         </div>
-        <!-- </div> -->
-      </div>
-      <div class="column is-6">
-        <img :src="$page.recipe.image" />
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column is-1"></div>
-      <div class="column is-10">
-        <div class="icon-and-text">
-          <b-icon icon="square" size="is-small"> </b-icon>
-          <h3>Steps</h3>
-        </div>
-        <div class="content">
-          <!-- <ul class="steps"> -->
-          <li v-for="(step, i) in $page.recipe.steps" :key="i">
-            {{ step.action }}
-          </li>
-          <!-- </ul> -->
+        <div class="column is-6">
+          <img :src="$page.recipe.image" />
         </div>
       </div>
-      <div class="column is-1"></div>
+      <div class="columns steps">
+        <div class="column is-10">
+          <p class="title is-4 ingredients-title">Steps</p>
+          <div class="content">
+            <ul class="steps">
+              <li v-for="(step, i) in $page.recipe.steps" :key="i">
+                {{ step.action }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
@@ -108,13 +96,32 @@ query ($id: ID!) {
 }
 </page-query>
 
-<style>
+<style scoped>
+.recipe-title {
+  margin-bottom: 0.2rem;
+}
 .recipe-content {
-  padding-left: 10%;
-  padding-right: 10%;
 }
 .icon-and-text {
   display: flex;
   align-items: center;
+}
+
+.ingredients-title {
+  margin-bottom: 0.2rem;
+}
+
+ul.ingredients {
+  list-style-type: disc;
+  margin-top: 0px;
+}
+
+ul.ingredients-no-amount {
+  list-style-type: disc;
+  margin-top: 0.25em;
+}
+
+ul.steps {
+  list-style-type: decimal;
 }
 </style>
