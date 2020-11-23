7a0c9af12f6c2875f5880a87c9faa58ca2b8a55a
serina <serina8910@gmail.com>
2020-11-23 22:58:22 +0900

    upd top page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,12 +4,46 @@
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">
-            Fullheight title
-          </h1>
-          <h2 class="subtitle">
-            Fullheight subtitle
+          <figure>
+            <img src="../assets/logo1_t.png">
+          </figure>
+          <!-- <h1 class="title is-1 has-text-weight-bold">
+            World Travel
+          </h1> -->
+          <h2 class="subtitle has-text-weight-semibold">
+            Would you like to go traveling with us?
           </h2>
+        </div>
+      </div>
+    </section>
+
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-8">
+              <div class="block">
+                <h1 class="title is-1 has-text-weight-bold">
+                  Our Concept
+                </h1>
+              </div>
+              <div class="block is-size-4">
+                <strong>"Bon"</strong>: good in French<br>
+              <!-- </div>
+              <div class="block is-size-2"> -->
+                <strong>"Tabio"</strong>: Travel/Trip + o(wo) in Japanese
+              </div>
+              <div class="block">
+                We love going traveling.<br>
+                We love eating.<br>
+                We love cooking.<br>
+                And we love sharing them with you.
+              </div>
+              <div class="block">
+                We hope you feel like traveling with our contents.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
