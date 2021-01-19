3871a37c0a6cd7ecbe6db41bb8f0d916f2e6e7fe
serina <serina8910@gmail.com>
2021-01-19 23:13:49 +0900

    delete small icon pic

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -13,11 +13,6 @@
           </div>
           <div class="card-content">
             <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                </figure>
-              </div>
               <div class="media-content">
                 <p class="title is-4">Ado</p>
                 <p class="subtitle is-6">@ado</p>
@@ -46,11 +41,6 @@
           </div>
           <div class="card-content">
             <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                </figure>
-              </div>
               <div class="media-content">
                 <p class="title is-4">Seri</p>
                 <p class="subtitle is-6">@seri</p>
