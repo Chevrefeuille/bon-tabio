63220668ac1f12dfb4fcd21cb0f6aa46fa9c9f75
serina <serina8910@gmail.com>
2021-01-23 18:44:57 +0900

    update layouts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -42,6 +42,7 @@
               <div class="block">
                 We hope you feel like traveling with our contents.
               </div>
+              <button type="button"><i class="icon ion-md-lock"></i>sample button</button>
             </div>
           </div>
         </div>
@@ -53,5 +54,62 @@
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
+$ruler: 16px;
+$color-red: #AE1100;
+$color-bg:#FFF1EB;
+$color-shadow: #BABECC;
+$color-white: #FFF;
+
+
+button {
+    font-family: 'Montserrat', sans-serif;
+    letter-spacing: -0.2px;
+    font-size: $ruler;
+}
+
+button{
+  border: 0;
+  outline: 0;
+  font-size: $ruler;
+  border-radius: $ruler*20;
+  padding: $ruler;
+  background-color:$color-bg;
+  text-shadow: 1px 1px 0 $color-white;
+}
+button {
+  color:#61677C;
+  font-weight: bold;
+  box-shadow: -5px -5px 20px $color-white,  5px 5px 20px $color-shadow;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+  
+  &:hover {
+    box-shadow: -2px -2px 5px $color-white, 2px 2px 5px $color-shadow;
+  }
+  
+  &:active {
+    box-shadow: inset 1px 1px 2px $color-shadow, inset -1px -1px 2px $color-white;
+  }
+  
+  
+  &.unit {
+    border-radius: $ruler/2;
+    line-height: 0;
+    width: $ruler*3;
+    height: $ruler*3;
+    display:inline-flex;
+    justify-content: center;
+    align-items:center;
+    margin: 0 $ruler/2;
+    font-size: $ruler*1.2;
+  }
+  
+  &.red {
+    display: block;
+    width: 100%;
+    color:$color-red;
+  }
+}
 </style>
