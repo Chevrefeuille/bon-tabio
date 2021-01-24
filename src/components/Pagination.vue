4132aed80f5a76d886d8d9cf0b9cc828062d7633
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2021-01-24 12:06:54 +0100

    merge pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,84 +1,57 @@
 <template>
-    <section>
-        <hr>
-        <b-pagination
-            :total="total"
-            v-model="current"
-            :range-before="rangeBefore"
-            :range-after="rangeAfter"
-            :order="order"
-            :size="size"
-            :simple="isSimple"
-            :rounded="isRounded"
-            :per-page="perPage"
-            :icon-prev="prevIcon"
-            :icon-next="nextIcon"
-            aria-next-label="Next page"
-            aria-previous-label="Previous page"
-            aria-page-label="Page"
-            aria-current-label="Current page">
+  <section>
+    <b-pagination
+      :total="$page.recipes.totalCount"
+      :current="currentPage"
+      :per-page="perPage"
+    >
+      <template #default="props">
+        <b-pagination-button
+          :page="props.page"
+          :id="props.page.number"
+          tag="router-link"
+          :to="`/recipes/${sanitize(props.page.number)}`"
+        >
+        </b-pagination-button>
+      </template>
 
-            <template #default="props">
-                <b-pagination-button
-                    :page="props.page"
-                    :id="`page${props.page.number}`"
-                    tag="router-link"
-                    :to="`/documentation/pagination#page${props.page.number}`">
-                    {{ (props.page.number) }}
-                </b-pagination-button>
-            </template>
+      <template #previous="props">
+        <b-pagination-button
+          :page="props.page"
+          tag="router-link"
+          :to="`/${ressourceName}/${sanitize(props.page.number)}`"
+        >
+        </b-pagination-button>
+      </template>
 
-
-            <!-- <template #previous="props">
-                <b-pagination-button
-                    :page="props.page"
-                    tag="router-link"
-                    :to="`/documentation/pagination#page${props.page.number}`">
-                    Previous
-                </b-pagination-button>
-            </template>
-
-            <template #next="props">
-                <b-pagination-button
-                    :page="props.page"
-                    tag="router-link"
-                    :to="`/documentation/pagination#page${props.page.number}`">
-                    Next
-                </b-pagination-button>
-            </template> -->
-
-        </b-pagination>
-    </section>
+      <template #next="props">
+        <b-pagination-button
+          :page="props.page"
+          tag="router-link"
+          :to="`/${ressourceName}/${sanitize(props.page.number)}`"
+        >
+        </b-pagination-button>
+      </template>
+    </b-pagination>
+  </section>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                total: 200,
-                current: 10,
-                perPage: 10,
-                rangeBefore: 1,
-                rangeAfter: 1,
-                order: 'is-centered',
-                size: '',
-                isSimple: false,
-                isRounded: true,
-                prevIcon: 'chevron-left',
-                nextIcon: 'chevron-right'
-            }
-        },
-        methods: {
-        },
-        watch: {
-            $route: {
-                immediate: true,
-                handler(newVal, oldVal) {
-                    if (newVal.hash) {
-                        this.current = parseInt(newVal.hash.replace(/\#page/g, ''))
-                    }
-                },
-            },
-        }
-    }
+export default {
+  props: ["ressourceName", "perPage"],
+  computed: {
+    currentPage() {
+      return this.$page.recipes.pageInfo.currentPage;
+    },
+  },
+  methods: {
+    // because recipes/1 gives a 404
+    sanitize(pageID) {
+      if (pageID == 1) {
+        return "";
+      }
+      return pageID;
+    },
+  },
+};
 </script>
--- src/pages/Recipes.vue
+++ src/pages/Recipes.vue
@@ -16,88 +16,33 @@
           </div>
         </div>
         <div class="pager">
-          <b-pagination
-            :total="$page.recipes.totalCount"
-            :current="currentPage"
-            :per-page="3"
-          >
-            <template #default="props">
-              <b-pagination-button
-                :page="props.page"
-                :id="props.page.number"
-                tag="router-link"
-                :to="`/recipes/${sanitize(props.page.number)}`"
-              >
-              </b-pagination-button>
-            </template>
-
-            <template #previous="props">
-              <b-pagination-button
-                :page="props.page"
-                tag="router-link"
-                :to="`/recipes/${sanitize(props.page.number)}`"
-              >
-              </b-pagination-button>
-            </template>
-
-            <template #next="props">
-              <b-pagination-button
-                :page="props.page"
-                tag="router-link"
-                :to="`/recipes/${sanitize(props.page.number)}`"
-              >
-              </b-pagination-button>
-            </template>
-          </b-pagination>
-          <!-- <b-pagination
-            
-            aria-next-label="Next page"
-            aria-previous-label="Previous page"
-            aria-page-label="Page"
-            aria-current-label="Current page"
-          >
-          </b-pagination> -->
+          <Pagination :ressourceName="recipes" :perPage="9" />
         </div>
       </div>
-      <pagination/>
     </section>
   </Layout>
 </template>
 
 <script>
 import RecipeCard from "~/components/RecipeCard.vue";
-import { Pager } from "gridsome";
-import Pagination from '~/components/Pagination.vue'
+import Pagination from "~/components/Pagination.vue";
 
 export default {
   components: {
     RecipeCard,
-    Pager,
-    Pagination
+    Pagination,
   },
   computed: {
-    currentPage() {
-      return this.$page.recipes.pageInfo.currentPage;
-    },
     recipes() {
       return this.$page.recipes.edges.map(({ node }) => node);
     },
   },
-  methods: {
-    // because recipes/1 gives a 404
-    sanitize(pageID) {
-      if (pageID == 1) {
-        return '';
-      }
-      return pageID;
-    }
-  }
 };
 </script>
 
 <page-query>
 query ($page: Int) {
-  recipes: allRecipe(perPage: 3, page: $page) @paginate {
+  recipes: allRecipe(perPage: 9, page: $page) @paginate {
     totalCount
     pageInfo {
       totalPages
